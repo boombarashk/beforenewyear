1b4b58840512cc23841dedb862011b65f977edae
boombarashk <boombarashk@yandex.ru>
2022-10-11 18:04:29 +0300

    update background

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './image.jpg'
+
 // import '../public/font/Xiomara.woff'
 // import '../public/font/Xiomara.woff2'
 import App from './App';
